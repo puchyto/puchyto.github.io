767810bc6945bf74d63ba38e993c4ab4c5fb9fd0
puchyto <luchytochikito@gmail.com>
2024-03-29 18:45:18 -0300

    pa mi wawa

---
+++ index.js
@@ -0,0 +1,74 @@
+window.sr = ScrollReveal();
+sr.reveal('#titulo', {
+    duration: 1500,
+    origin: 'bottom',
+    distance: '-100px'
+});
+sr.reveal('#ul', {
+    duration: 1500,
+    origin: 'left',
+    distance: '-10px'
+});
+sr.reveal('#ul2', {
+    duration: 1500,
+    origin: 'right',
+    distance: '-10px'
+});
+sr.reveal('#parrafito', {
+    duration: 1650,
+});
+sr.reveal('#ratita', {
+    duration: 7000,
+    rotate: {
+        x: 80,
+        y: 31,
+        z: 20,
+    }
+});
+sr.reveal('#c1', {
+    duration: 5000,
+    rotate: {
+        x: 30,
+        y: 11,
+        z: 10,
+    }
+});
+sr.reveal('#c2', {
+    duration: 5000,
+    rotate: {
+        x: 30,
+        y: 11,
+        z: 10,
+    }
+});
+sr.reveal('#abrazo', {
+    duration: 4000,
+    origin: 'right',
+    distance: '30px',
+});
+sr.reveal('#uwu', {
+    duration: 3000,
+    distance: '10px',
+});
+sr.reveal('#trauma', {
+    duration: 4000,
+    rotate: {
+        x: 40,
+        y: 2,
+    }
+});
+sr.reveal('#bb', {
+    duration: 9070,
+    rotate: {
+        x: 100,
+        y: 2,
+    }
+});
+sr.reveal('#chingue', {
+    duration: 9000,
+    rotate: {
+        x: 100,
+        y: 2,
+        z: 40,
+    }
+});
